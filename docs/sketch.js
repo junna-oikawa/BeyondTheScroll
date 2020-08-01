6c3dbf31e334e2237a96fb7e864c501c7eefe0f2
Junna Oikawa <jnn.y326@gmail.com>
2020-08-01 17:52:02 +0900

    トップページと音

--- docs/sketch.js
+++ docs/sketch.js
@@ -3,77 +3,134 @@ let musicBox;
 let cnv;
 let Y;
 let u;
-let speed = 200;
-let mbPlayMode = false;
+let comparisonY;
+let volume = 0;
+
+let Ytop;
+let img = [];
+let alpha = 0;
+let size;
 
 function preload() {
   springSound = loadSound('spring.mp3');
   musicBox = loadSound('musicbox.mp3');
+
+  for (var i = 0; i <62; i++){
+      img[i] = loadImage('./image/img' + [i] + '.jpg');
+  }
+}
+
+function touchStarted() {
+  getAudioContext().resume();
 }
 
 function setup() {
-  // document.getElementById('main').innerHTML = new Array(1000).fill(0).map((d,i) => `${i+1}行目…………………………`).join('<br />');
   cnv = createCanvas(window.innerWidth, window.innerHeight);
-  rectMode(CENTER);
-  colorMode(HSB, 360, 100, 100);
+
+  imageMode(CENTER);
+  image(img[0], window.innerWidth/2, window.innerHeight/2 - window.innerWidth*0.1, window.innerWidth, window.innerWidth/1.8);
   loop();
+  setTimeout(setComparison,30);
 }
 
 
 function draw() {
   let scrollY = window.scrollY;
-  Y = floor(scrollY);
+  Y = floor(scrollY)- 600;
   background(255);
+
+  let w = window.innerWidth;
+  let h = window.innerHeight;
+  Ytop = floor(scrollY);
+//   image(img[1], 0, 0);
+
+  for( var i = 0; i <62; i++) {
+    if(Ytop >= i*10) {
+     image(img[i], w/2, h/2- w*0.1, size, size/1.8);
+    }
+  }
+  print(Ytop);
+
+  if ( Ytop >= 230 ){
+    alpha = Ytop - 230;
+    // size = w + (Ytop - 230)*5;
+  } else {
+      alpha = 0;
+      //size = w;
+  }
+
+  if (Ytop >=300 ) {
+    size = w + (Ytop - 300)*5;
+  } else {
+    size = w;
+  }
+  fill(255, 255, 255, alpha);
+  rect(0, 0, window.innerWidth, window.innerHeight);
 
   fill(0);
   textSize(50);
   text(Y, 100, 100);
-  
-  if (mbPlayMode == true) {
-    t();
-  }
-  if (mbPlayMode !=true) {
-    f();
+
+  if (scrollY >=600) {
+    scrollMain();
   }
 }
 
-function mouseClicked(){
-  if (mbPlayMode == true) {
-    mbPlayMode = false;
-  }
-  else if (mbPlayMode == false) {
-    mbPlayMode = true;
-  }
-  print(mbPlayMode);
+function scrollMain() {
+  comapare();
+  playMusicSound();
 }
 
-function t() {
-  u = Y;
-  springSound.stop();
-  musicBox.rate(1);
-  if(musicBox.isPlaying() == false){
-    musicBox.play();
-  }
-  u = u-5;
-  print(u);
-  window.scrollTo(0,u);
-  if (u <= 0) {
+function playMusicSound() {
+  if (volume > 0) {
+    musicBox.amp(volume);
+    if(musicBox.isPlaying() == false){
+      
+      musicBox.play();
+    }
+  } else if(volume <= 0){
     musicBox.pause();
-    mbPlayMode = false;
   }
 }
 
-function f() {
-  musicBox.pause();
-  if(Y%20 == 0 && Y != 0) {
-    if(springSound.isPlaying() == false){
-      springSound.rate(2);
+function setComparison() {
+  comparisonY = Y;
+  setTimeout(setComparison,30);
+}
+
+function comapare() {
+  // volume++;
+  //print(volume);
+  if (Y <= comparisonY && Y > 0) {
+    u = u-1;
+    if(u <= 0) {
+      u = 600;
+    }
+    //print(u);
+    window.scrollTo(0,u+600);
+    volume += 0.005;
+    if (volume >= 1) {
+      volume = 1;
+    }
+    springSound.stop();
+  } else if (Y > comparisonY){
+    u = Y;
+    volume -= 0.01;
+    if (volume <= 0) {
+      volume = 0;
+    }
+    if(springSound.isPlaying() == false && Y!=0){
       springSound.play();
+    }
+  } else if (Y <= 0) {
+    springSound.stop();
+    u = Y;
+    volume -= 0.005;
+    if (volume <= 0) {
+      volume = 0;
     }
   }
 }
-
-
 
 //ゼンマイの音
 //<http://;.net/sound/search/%E3%82%BC%E3%83%B3%E3%83%9E%E3%82%A4/>
